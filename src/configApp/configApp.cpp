e52a347e04ee5c0ba5217e20ac71db7ef3d68eb2
allivka <allivka1920@gmail.com>
2025-07-08 13:13:07 +0300

    wrote some shitty code for configApp

--- src/configApp/configApp.cpp
+++ src/configApp/configApp.cpp
@@ -15,6 +15,8 @@ bool isLMBPressed = false;
 
 cv::Vec3b getMatMedianHSV(cv::Mat mat) {
     
+    cv::cvtColor(mat, mat, cv::COLOR_BGR2HSV);
+    
     std::vector<uchar> h;
     std::vector<uchar> s;
     std::vector<uchar> v;
@@ -61,6 +63,9 @@ void createSettingsWindow() {
     cv::createTrackbar("highVal", "settings", &highVal, 255);
 }
 
+cv::Vec3b hsvVec;
+bool hsvReceived = false;
+
 static void mouseCallback(int event, int x, int y, int flags, void *data) {
     
     mousePos = cv::Point(x, y);
@@ -75,7 +80,8 @@ static void mouseCallback(int event, int x, int y, int flags, void *data) {
         isLMBPressed = false;
         activeRect = cv::Rect(LMBPressPos, mousePos);
         
-        cv::Vec3b hsvVec = getMatMedianHSV((*((cv::Mat*)data))(activeRect));
+        hsvVec = getMatMedianHSV((*((cv::Mat*)data))(activeRect));
+        hsvReceived = true;
         std::cout << (int)hsvVec[0] << '\t' << (int)hsvVec[1] << '\t' << (int)hsvVec[2] << '\n';
         
         break;
@@ -87,7 +93,9 @@ uint fps = 30;
 uint width = 960;
 uint height = 720;
 
-void initCaptureFromFile(const std::string& path, cv::VideoCapture& cap) {
+cv::VideoCapture cap;
+
+void initCaptureFromFile(const std::string& path) {
     std::ifstream in(path);
     
     in >> fps;
@@ -105,16 +113,51 @@ void initCaptureFromFile(const std::string& path, cv::VideoCapture& cap) {
     
 }
 
+void highLightBall() {
+    
+    int key = -1;
+    
+    cv::Mat instrFrame = cv::Mat::zeros(300, 300, CV_8UC3);
+    
+    cv::putText(instrFrame, "Highlight the ball", cv::Point(10, 30), cv::FONT_ITALIC, 1.0, cv::Scalar(0, 0, 100), 2);
+    
+    cv::Mat frame, drawFrame;
+    
+    cv::setMouseCallback("1", mouseCallback, &frame);
+    
+    while(key != 27) {
+        key = cv::waitKey(1000 / fps);
+        cv::imshow("instruction", instrFrame);
+        
+        cap >> frame;
+        
+        cv::GaussianBlur(frame, frame, cv::Size(7, 7), 1, 1);
+        drawFrame = frame;
+        if(isLMBPressed) cv::rectangle(drawFrame, cv::Rect(LMBPressPos, mousePos), cv::Scalar(100, 0, 0), 10); 
+        cv::imshow("1", drawFrame);
+        
+        if(hsvReceived) {
+            std::ofstream out("data/server/config.txt", std::ios::app);
+            out << (unsigned int)hsvVec[0] << '\t' << (unsigned int)hsvVec[1] << '\t' << (unsigned int)hsvVec[2] << '\n';
+            hsvReceived = false;
+            return;
+        }
+        
+    }
+    
+}
+
 int main(int argc, const char *argv[]) {
     int id = 0;
     
     if(argc == 2) id = atoi(argv[1]);
     
-    cv::VideoCapture cap;
+    std::ofstream out("data/server/config.txt", std::ios::trunc);
+    out.close();
     
     cap.open(id);
     
-    initCaptureFromFile("data/configApp/config.txt", cap);
+    initCaptureFromFile("data/configApp/config.txt");
     
     cv::Mat frame1;
     cv::Mat frame2;
@@ -122,59 +165,52 @@ int main(int argc, const char *argv[]) {
     cv::Mat gray;
     cv::Mat hsv;
     
+    createSettingsWindow();
+    cv::namedWindow("instruction", cv::WINDOW_NORMAL);
+    
     cv::namedWindow("1", cv::WINDOW_NORMAL);
     cv::namedWindow("2" , cv::WINDOW_NORMAL);
     cv::namedWindow("3", cv::WINDOW_NORMAL);
     
-    cv::setMouseCallback("1", mouseCallback, &frame1);
     
-    cv::moveWindow("1", 100, 100);
-    cv::moveWindow("2", 1000, 100);
-    cv::moveWindow("3", 100, 500);
+    highLightBall();
     
-    int key = -1;
     
-    while(key != 27) {
-        key = cv::waitKey(1000 / fps);
+    // int key = -1;
+    
+    // while(key != 27) {
+    //     key = cv::waitKey(1000 / fps);
         
-        if(key == 115) {
-            createSettingsWindow();
-        } else if(key == 99) {
-            cv::destroyWindow("settings");
-        }
+    //     cap >> frame1;
         
-        cap >> frame1;
+    //     cv::GaussianBlur(frame1, frame2, cv::Size(7, 7), 1, 1);
         
-        cv::GaussianBlur(frame1, frame2, cv::Size(7, 7), 1, 1);
+    //     cv::cvtColor(frame2, hsv, cv::COLOR_BGR2HSV);
         
-        cv::cvtColor(frame2, hsv, cv::COLOR_BGR2HSV);
+    //     cv::inRange(hsv, cv::Scalar(lowHue, lowSat, lowVal), cv::Scalar(highHue, highSat, highVal), frame2);
         
-        cv::inRange(hsv, cv::Scalar(lowHue, lowSat, lowVal), cv::Scalar(highHue, highSat, highVal), frame2);
+    //     cv::Moments m = cv::moments(frame2);
+    //     double k = m.m00;
         
-        cv::Moments m = cv::moments(frame2);
-        double k = m.m00;
+    //     if(k == 0) continue;
         
-        if(k == 0) continue;
+    //     double xC = m.m10 / k;
+    //     double yC = m.m01 / k;
         
-        double xC = m.m10 / k;
-        double yC = m.m01 / k;
+    //     cv::cvtColor(frame2, frame2, cv::COLOR_GRAY2BGR);
         
-        cv::cvtColor(frame2, frame2, cv::COLOR_GRAY2BGR);
+    //     frame3 = frame1 & frame2;
         
-        frame3 = frame1 & frame2;
+    //     cv::circle(frame1, cv::Point(xC, yC), 50, cv::Scalar(0, 0, 255), 10);
         
-        cv::circle(frame1, cv::Point(xC, yC), 50, cv::Scalar(0, 0, 255), 10);
+    //     
         
-        if(isLMBPressed) cv::rectangle(frame1, cv::Rect(LMBPressPos, mousePos), cv::Scalar(100, 0, 0), 10);
+    //     cv::imshow("1", frame1);
         
+    //     cv::imshow("2", frame2);
         
-        
-        cv::imshow("1", frame1);
-        
-        cv::imshow("2", frame2);
-        
-        cv::imshow("3", frame3);
-    }
+    //     cv::imshow("3", frame3);
+    // }
     
     cv::destroyAllWindows();
     
