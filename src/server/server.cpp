cdb20c71a06c2957c1686b4a295da1b6d912d0b8
allivka <allivka1920@gmail.com>
2025-07-10 10:57:11 +0300

    config

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -55,7 +55,7 @@ int main(int argc, char *argv[]) {
 
         Command com;
         com.isKick = 0;
-        com.direction = (bool)(speed < 0);
+        com.direction = (bool)(speed > 0);
         com.setIntensity((uint8_t)std::abs(speed));
         
         if(std::abs(points.ballPos.value().y - points.robotPos.value().y) <= 100 && std::abs(points.ballPos.value().x - points.robotPos.value().x) <= 100) {
